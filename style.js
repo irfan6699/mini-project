4463d5ff41c7ddaf303b2dd0bad43c0fcdad0978
Jafri Irfan <jafriirfan36@gmail.com>
2024-09-12 22:23:51 +0530

    files

---
+++ style.js
@@ -0,0 +1,24 @@
+const slider=document.querySelector(`.slider`);
+ let count=0
+function changeImage(){
+    const imagecon=["slider1.jpg","slider2.jpg"];
+    count=(count+1)%imagecon.length;
+      slider.style.backgroundImage=`url('${imagecon[count]}')`;
+      
+    };
+setInterval(changeImage,3000);
+
+const changes = document.querySelectorAll('.change');
+changes.forEach(change => {
+    change.addEventListener('click', () => {
+        if (change.src.includes('black.png')) {
+            change.src = 'Heart.png';
+        } else {
+            change.src = 'black.png';
+        }
+    });
+});
+const directTra=document.getElementById('tra');
+directTra.addEventListener('click',function(){
+    window.location.href="traditional.html";
+})
